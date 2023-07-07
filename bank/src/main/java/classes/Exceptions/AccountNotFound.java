ca5aeba4a314683f395b3498a75bc9ddce50407d
johmartinss <johndodo07@gmail.com>
2023-07-07 15:22:20 -0300

    deletaaaaaaaaaaaaaaaar

--- bank/src/main/java/classes/Exceptions/AccountNotFound.java
+++
@@ -1,7 +0,0 @@
-package classes.Exceptions;
-
-public class AccountNotFound extends Exception {
-    public AccountNotFound(){
-        super("Nao tem uma conta vinculado com seu usuario");
-    }
-}
